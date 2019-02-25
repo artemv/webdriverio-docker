8dadca8370012dd0e541b65246be209fa4c155a7
Artem Vasiliev <artem.job@gmail.com>
2019-02-25 14:24:31 +0300

    fix: corrected re caddy port

--- test/specs/caddy.js
+++ test/specs/caddy.js
@@ -3,7 +3,8 @@ import {expect} from "chai";
 describe("caddy", () => {
 
     it("works", () => {
-        browser.url(`http://${process.env.CADDY_SERVER}/`);
+        const CADDY_PORT = 2015;
+        browser.url(`http://${process.env.CADDY_SERVER}:${CADDY_PORT}/`);
         browser.waitForVisible('h1=Caddy web server');
     });
 
